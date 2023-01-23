be8165464263e984e122b7a873238c408ba724d0
laurence <laurence.jones@live.co.uk>
2023-01-23 10:11:53 +0000

    Remove cscli and sleep less

--- crowdsec/migrate.deb.sh
+++ crowdsec/migrate.deb.sh
@@ -3,7 +3,6 @@ sudo apt install postgresql postgresql-contrib -y
 sudo systemctl start postgresql.service
 TEMP_DIR=$(mktemp -d)
 RANDOM_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20)
-sudo cscli -oraw decisions list 2>1 > "$TEMP_DIR/decisions.csv"
 echo "db_config:
   type: pgx
   user: crowdsec
@@ -17,11 +16,10 @@ echo "db_config:
 #api:
   #server:
     #use_forwarded_for_headers: true" > /etc/crowdsec/config.yaml.local
-sleep 5
+sleep 3
 sudo -i -u postgres psql -c "CREATE DATABASE crowdsec;"
 sudo -i -u postgres psql -c "CREATE USER crowdsec WITH PASSWORD '$RANDOM_PASS';"
 sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE crowdsec TO crowdsec;"
 sudo cscli machines add -a
 sudo systemctl restart crowdsec.service
-sudo cscli decisions import -i "$TEMP_DIR/decisions.csv"
 rm -rf $TEMP_DIR
