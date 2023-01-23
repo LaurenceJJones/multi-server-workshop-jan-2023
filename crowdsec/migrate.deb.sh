2a405c815484176864a4ce514170bac82a60a1b2
laurence <laurence.jones@live.co.uk>
2023-01-23 10:12:16 +0000

    No need for temp dir

--- crowdsec/migrate.deb.sh
+++ crowdsec/migrate.deb.sh
@@ -1,7 +1,6 @@
 ##Only tested on ubuntu systems
 sudo apt install postgresql postgresql-contrib -y
 sudo systemctl start postgresql.service
-TEMP_DIR=$(mktemp -d)
 RANDOM_PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20)
 echo "db_config:
   type: pgx
@@ -22,4 +21,3 @@ sudo -i -u postgres psql -c "CREATE USER crowdsec WITH PASSWORD '$RANDOM_PASS';"
 sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE crowdsec TO crowdsec;"
 sudo cscli machines add -a
 sudo systemctl restart crowdsec.service
-rm -rf $TEMP_DIR
